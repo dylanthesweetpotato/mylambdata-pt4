627b0ad01ef722b92938f85c8a6c2fbdeeb9f737
Dylan Purchase <dylanthesweetpotato>
2020-03-13 21:53:35 -0400

    farted:

--- my_lambdata/my_mod.py
+++ my_lambdata/my_mod.py
@@ -3,3 +3,12 @@
 
 def enlarge(n):
     return n * 100
+
+
+
+#y = float(input("Please enter a number"))
+#print(enlarge(y))
+if __name__ == "__main__":
+
+    y = float(input("Please enter a number"))
+    print(enlarge(y))
