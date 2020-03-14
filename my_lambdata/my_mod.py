5084359412513c531be61ecf3510089e869510c3
Dylan Purchase <dylanthesweetpotato>
2020-03-13 21:42:09 -0400

    function

---
+++ my_lambdata/my_mod.py
@@ -0,0 +1,5 @@
+
+
+
+def enlarge(n):
+    return n * 100
--- my_lambdata/my_script.py
+++ my_lambdata/my_script.py
@@ -1,6 +1,12 @@
 import pandas as pd
 
+from my_lambdata.my_mod import enlarge
+
+
 print("Happy Wednesday")
 
 df = pd.DataFrame ({"x": [1,2,3], "y":[4,5,6]})
 print (df)
+
+x = 5
+print(x, "Enlarge", enlarge(x))
